caa074ef4ba09cffaf59198395b96b323d186832
lfernandomendonca <luiz.mendonca@al.infnet.edu.br>
2023-02-20 14:00:23 +0000

    teste

--- src/pages/anime.tsx
+++ src/pages/anime.tsx
@@ -3,31 +3,31 @@ import { useEffect, useState } from "react";
 
 
 
-const Anime = () => {
+// const Anime = () => {
     
-    const [animeList, setAnimeList] = useState([]);
+//     const [animeList, setAnimeList] = useState([]);
 
-    const callAPI = async () => {
-        try{
-            const res = await fetch(`https://gogoanime.consumet.stream/popular`, {method:'GET'})
-            const data = await res.json()
-            setAnimeList(data)
-            console.log(data);
-        } catch(error){
-            console.log(error);
-        }
-    }
+//     const callAPI = async () => {
+//         try{
+//             const res = await fetch(`https://gogoanime.consumet.stream/popular`, {method:'GET'})
+//             const data = await res.json()
+//             setAnimeList(data)
+//             console.log(data);
+//         } catch(error){
+//             console.log(error);
+//         }
+//     }
     
-    useEffect(()=> {
-        callAPI();
-    });
-    return(
-    <ul>
-       {animeList.map((anime) => (
-        <><li>{anime.animeTitle}</li><img src={anime.animeImg} alt="" /></>
-       ))}
-    </ul>
+//     useEffect(()=> {
+//         callAPI();
+//     });
+//     return(
+//     <ul>
+//        {animeList.map((anime) => (
+//         <><li>{anime.animeTitle}</li><img src={anime.animeImg} alt="" /></>
+//        ))}
+//     </ul>
 
-    )
-} 
-// export default Anime
+//     )
+// } 
+// // export default Anime
